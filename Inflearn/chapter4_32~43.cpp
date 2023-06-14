9492106fe6abd26ff6edec1837f74543279cf5c3
SsongSik <sgsk88@nate.com>
2023-06-14 19:53:16 +0900

    [inflearn] #32 선택정렬

---
+++ Inflearn/chapter4_32~43.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+
+int main() {
+    /*
+     * 32번 : 선택정렬
+     * ex) 6
+     * 13 5 11 7 23 15
+     * -> 5 7 11 13 15 23
+     *  int a[100], n, idx, i, j, tmp;
+
+        std::cin>>n;
+        for(i=0; i<n; i++) {
+            std::cin>>a[i];
+        }
+        for(i=0; i<n-1; i++) {
+            idx=i;
+            for(j=i+1; j<n; j++) {
+                if(a[idx]>a[j]) {
+                    idx=j;
+                }
+            }
+            tmp=a[i];
+            a[i]=a[idx];
+            a[idx]=tmp;
+        }
+        for(i=0; i<n; i++) {
+            std::cout<<a[i]<<' ';
+        }
+     */
+}
