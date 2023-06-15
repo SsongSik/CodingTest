6368dcd06125d7a4657e554eda68ee5e0ce195b8
SsongSik <sgsk88@nate.com>
2023-06-15 23:03:32 +0900

    [inflearn] #33 3등의 성적은? (정렬 응용)

--- Inflearn/chapter4_32~43.cpp
+++ Inflearn/chapter4_32~43.cpp
@@ -31,4 +31,41 @@ int main() {
             std::cout<<a[i]<<' ';
         }
      */
+    /*
+     * 33번 : 3등의 성적은?(정렬 응용)
+     * ex) 7
+     * 80 96 75 82 96 92 100
+     * -> 92
+     *  int n, cnt=0, res;
+        std::cin>>n;
+        std::vector<int> a(n);
+
+        int i, j, idx, tmp;
+        for(i=0; i<n; i++) {
+            std::cin>>a[i];
+        }
+
+        for(i=0; i<n-1; i++) {
+            idx=i;
+            for(j=i+1; j<n; j++) {
+                if(a[idx]<a[j]) {
+                    idx=j;
+                }
+            }
+            tmp=a[idx];
+            a[idx]=a[i];
+            a[i]=tmp;
+        }
+
+        for(i=1; i<n; i++) {
+            if(a[i-1]!=a[i]) {
+                cnt++;
+            }
+            if(cnt==2) {
+                res=a[i];
+                break;
+            }
+        }
+        std::cout<<res;
+     */
 }
