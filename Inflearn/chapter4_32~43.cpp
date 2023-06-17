d3070755838ec2004540266ed633bf9ef3d3b37b
SsongSik <sgsk88@nate.com>
2023-06-17 15:11:15 +0900

    [inflearn] #34 버블정렬

--- Inflearn/chapter4_32~43.cpp
+++ Inflearn/chapter4_32~43.cpp
@@ -68,4 +68,31 @@ int main() {
         }
         std::cout<<res;
      */
+    /*
+     * 34번 : 버블정렬
+     * ex) 6
+     * 13 23 11 7 5 15
+     * -> 5 7 11 13 15 23
+     *  int n, i, j, tmp;
+        std::vector<int> a(n);
+        std::cin>>n;
+
+        for(i=0; i<n; i++) {
+            std::cin>>a[i];
+        }
+
+        for(i=0; i<n-1; i++) {
+            for(j=0; j<n-i-1; j++) {
+                if(a[j]>a[j+1]) {
+                    tmp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=tmp;
+                }
+            }
+        }
+
+        for(i=0; i<n; i++) {
+            std::cout<<a[i]<<' ';
+        }
+     */
 }
