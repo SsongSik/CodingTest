b694add9d80fb386c6d25da55bb9b76c6cea54f2
SsongSik <sgsk88@nate.com>
2023-07-06 23:25:06 +0900

    [inflearn] #40 교집합

--- Inflearn/chapter4_32~43.cpp
+++ Inflearn/chapter4_32~43.cpp
@@ -238,4 +238,35 @@ int main() {
             std::cout<<c[i]<<' ';
         }
      */
+    /*
+     * 40번 교집합
+     *  int n, m, i, p1=0, p2=0, p3=0;
+        std::cin>>n;
+        std::vector<int> a(n);
+        for(i=0; i<n; i++){
+            std::cin>>a[i];
+        }
+        sort(a.begin(), a.end());
+
+        std::cin>>m;
+        std::vector<int> b(m), c(m);
+        for(i=0; i<m; i++){
+            std::cin>>b[i];
+        }
+        sort(b.begin(), b.end());
+
+        while(p1<n && p2<m){
+            if(a[p1]==b[p2]){
+                c[p3++]=a[p1++];
+                p2++;
+            }
+            else if(a[p1]<b[p2]){
+                p1++;
+            }
+            else p2++;
+        }
+        for(i=0; i<p3; i++){
+            printf("%d ", c[i]);
+        }
+     */
 }
