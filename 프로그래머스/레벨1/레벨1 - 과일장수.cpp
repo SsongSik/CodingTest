744310b0a70e807ad4aca77848002f63cc8a9d4d
sgsk88 <sgsk88@nate.com>
2023-09-11 23:04:58 +0900

    레벨1(과일장수)

---
+++ 프로그래머스/레벨1/레벨1 - 과일장수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+
+    sort(score.rbegin(), score.rend());
+    int n = score.size();
+
+    for(int i = m-1; i < n; i+=m) {
+        answer += (m * score[i]);
+    }
+
+    return answer;
+}
