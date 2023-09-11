f59a43b4d64fea6c60cf06988d5e1a611a49a160
sgsk88 <sgsk88@nate.com>
2023-09-11 23:55:29 +0900

    레벨1(자릿수 더하기)

--- 프로그래머스/레벨1/레벨1 - 자릿수 더하기.cpp
+++ 프로그래머스/레벨1/레벨1 - 자릿수 더하기.cpp
@@ -18,3 +18,16 @@ int solution(int n)
 
     return answer;
 }
+
+int solution2(int n)
+{
+    int answer = 0;
+
+    string s = to_string(n);
+
+    for(int i=0; i<s.size(); i++) {
+        answer += (s[i] - '0');
+    }
+
+    return answer;
+}
