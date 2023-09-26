ef536da69838d774ee7084f46a183e6d460e6e78
sgsk88 <sgsk88@nate.com>
2023-09-26 22:08:58 +0900

    레벨1(내적)

---
+++ 프로그래머스/레벨1/레벨1 - 내적.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> a, vector<int> b) {
+    int answer = 0;
+    int tmp = 0;
+
+    for(int i=0; i<a.size(); i++) {
+        tmp = a[i] * b[i];
+        answer += tmp;
+    }
+
+    return answer;
+}
